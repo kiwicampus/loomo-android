ae30f63892e3e29483e6c39aa2dee9230bde9859
josancamon19 <joan.santiago.cabezas@gmail.com>
2020-04-09 14:35:45 -0500

    Freedom tokens updated

--- app/src/main/java/com/kiwicampus/loomo/utils/Constants.kt
+++ app/src/main/java/com/kiwicampus/loomo/utils/Constants.kt
@@ -3,10 +3,15 @@ package com.kiwicampus.loomo.utils
 class Constants {
     companion object {
         const val FREEDOM_BASE_URL = "https://api.freedomrobotics.ai/"
-        const val FREEDOM_TOKEN = "Tfca08532005ce39cff4e663f"
-        const val FREEDOM_SECRET = "Sc7f1bec4bfff664b879e5f2c"
+        const val FREEDOM_TOKEN = "T2c715825b83074fcf7767c79"
+        const val FREEDOM_SECRET = "S4ea6b53b7638a39cb8214b85"
         const val FREEDOM_ACCOUNT = "A366ACF49B345A5C2E2742E1E"
         const val FREEDOM_DEVICE = "D79E0D5B155521B22344E2E6A6C"
+
+        const val FREEDOM_TOKEN_DEV = "T2c715825b83074fcf7767c79"
+        const val FREEDOM_SECRET_DEV = "S4ea6b53b7638a39cb8214b85"
+        const val FREEDOM_ACCOUNT_DEV = "A366ACF49B345A5C2E2742E1E"
+        const val FREEDOM_DEVICE_DEV = "D79E0D5B155521B22344E2E6A6C"
 
 
     }
